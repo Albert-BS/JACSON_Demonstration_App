ff3c9965e3ee2c11603a5c885af4bedd62655640
Albert-BS <albertbertranserrano@gmail.com>
2023-06-24 17:41:34 +0200

    Done GG Finito :)

--- app/src/main/java/com/example/uploadretrieveimage/MyAdapter.java
+++ app/src/main/java/com/example/uploadretrieveimage/MyAdapter.java
@@ -34,12 +34,13 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.TimeZone;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 
 public class MyAdapter {
-    private DatabaseReference database;
+    private final DatabaseReference database;
     String username, imageURL;
-    private String country;
+    private final String country;
 
     public MyAdapter(String username, String imageURL, String country) {
         this.database = FirebaseDatabase.getInstance().getReference();
@@ -59,14 +60,11 @@ public class MyAdapter {
         CompletableFuture<Void> attributesFuture = new CompletableFuture<>();
 
         final String[] policyJson = new String[1];
-
-        // Get the user key using the username
         Query userQuery = usersRef[0].orderByChild("username").equalTo(username);
         userQuery.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) {
-                    String userKey = dataSnapshot.getChildren().iterator().next().getKey();
                     HashMap<String, Object> userData = (HashMap<String, Object>) dataSnapshot.getChildren().iterator().next().getValue();
                     Map<String, String> map = new HashMap<>();
                     for (Map.Entry<String, Object> entry : userData.entrySet()) {
@@ -77,7 +75,7 @@ public class MyAdapter {
                     Query imageQuery = imagesRef[0].orderByChild("imageURL").equalTo(imageURL);
                     imageQuery.addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
-                        public void onDataChange(DataSnapshot dataSnapshot) {
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                             if (dataSnapshot.exists()) {
                                 String imageId = dataSnapshot.getChildren().iterator().next().getKey();
                                 DataSnapshot imageSnapshot = dataSnapshot.child(imageId);
@@ -85,61 +83,41 @@ public class MyAdapter {
                                     String caption = imageSnapshot.child("caption").getValue(String.class);
                                     captionFuture.complete(caption);
                                 }
-
-                                // Get the policy ID for the image
                                 DatabaseReference imageRef = imagesRef[0].child(imageId);
                                 imageRef.addListenerForSingleValueEvent(new ValueEventListener() {
                                     @Override
-                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                                         if (dataSnapshot.exists() && dataSnapshot.hasChild("policyId")) {
                                             String policyId = dataSnapshot.child("policyId").getValue(String.class);
                                             policyIdFuture.complete(policyId);
-
-                                            // Get the policy using the policy ID
                                             DatabaseReference policyRef = policiesRef.child(policyId);
                                             policyRef.addListenerForSingleValueEvent(new ValueEventListener() {
                                                 @Override
-                                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                                                     if (dataSnapshot.exists()) {
-                                                        // Process the policy JSON
                                                         policyJson[0] = dataSnapshot.getValue(String.class);
-                                                        // Parse the JSON into a Java object (you can use a JSON library like Jackson or Gson)
-                                                        // Process the conditions in the policy
                                                         processJSON(policyJson[0], attributes, map);
                                                         attributesFuture.complete(null);
                                                     }
                                                 }
-
                                                 @Override
-                                                public void onCancelled(DatabaseError databaseError) {
-                                                    // Handle the error
-                                                }
+                                                public void onCancelled(@NonNull DatabaseError databaseError) {}
                                             });
                                         }
                                     }
-
                                     @Override
-                                    public void onCancelled(DatabaseError databaseError) {
-                                        // Handle the error
-                                    }
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {}
                                 });
                             }
                         }
-
                         @Override
-                        public void onCancelled(DatabaseError databaseError) {
-                            // Handle the error
-                        }
+                        public void onCancelled(@NonNull DatabaseError databaseError) {}
                     });
                 }
             }
-
             @Override
-            public void onCancelled(DatabaseError databaseError) {
-                // Handle the error
-            }
+            public void onCancelled(@NonNull DatabaseError databaseError) {}
         });
-
         try {
             CompletableFuture.allOf(captionFuture, policyIdFuture, attributesFuture).get();
         } catch (InterruptedException | ExecutionException e) {
@@ -148,43 +126,56 @@ public class MyAdapter {
 
         String caption = captionFuture.join();
         String policyId = policyIdFuture.join();
-
         DatabaseReference requestsRef = database.child("Requests");
         final long[] requestsCount = {0};
+        CountDownLatch latch = new CountDownLatch(1);
+        String requestJson;
         requestsRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 requestsCount[0] = snapshot.getChildrenCount();
+                latch.countDown();
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
+                latch.countDown();
             }
         });
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         Map<String, Object> request = new LinkedHashMap<>();
         request.put("returnPolicyIdList", false);
         request.put("schemaLocation", "http://json-schema.org/draft-06/schema");
         request.put("desc", "Request to access image " + (caption != null ? caption : ""));
         String policyIdValue = (policyId != null ? policyId : "");
-        //String modifiedPolicyIdValue = policyIdValue.replace("policy", "request");
-        request.put("requestId", "request" + requestsCount[0]);
+        request.put("requestId", "request" + (requestsCount[0]+1));
         request.put("attributes", attributes);
 
         Map<String, Object> wrappedRequest = new LinkedHashMap<>();
         wrappedRequest.put("Request", request);
 
         ObjectMapper objectMapper = new ObjectMapper();
-        String requestJson;
         try {
             requestJson = objectMapper.writeValueAsString(wrappedRequest);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
 
-        String requestId = "Request" + (requestsCount[0] + 1);
+        String requestId = "request" + (requestsCount[0] + 1);
         requestsRef.child(requestId).setValue(requestJson);
 
-        String response = Validator.checkPolicy(requestJson, policyJson[0]);
+        String response;
+        if (requestJson.isEmpty()) {
+            response = "{\"results\":[{\"result\":\"Permit\"}]}";
+        }
+        else {
+            response = Validator.checkPolicy(requestJson, policyJson[0]);
+        }
         JSONObject responseJson = new JSONObject(response);
         JSONArray resultsArray = responseJson.getJSONArray("results");
         if (resultsArray.length() > 0) {
@@ -209,7 +200,6 @@ public class MyAdapter {
     private void processJSON(String policyString, List<Map<String, Object>> attributes, Map<String, String> map) {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
         try {
             JsonNode policyNode = objectMapper.readTree(policyString);
             processConditions(policyNode, attributes, map);
@@ -240,10 +230,13 @@ public class MyAdapter {
             for (JsonNode nestedApply : applyNode) {
                 JsonNode attributeValueNode = nestedApply.path("attributeValue");
                 JsonNode attributeDesignatorNode = nestedApply.path("attributeDesignator");
-
                 String dataType = attributeValueNode.path("dataType").asText();
                 String category = attributeDesignatorNode.path("category").asText();
                 String attributeId = attributeDesignatorNode.path("attributeId").asText();
+                if (!map.containsKey(attributeId)) {
+                    processApply(nestedApply.path("apply"), attributes, map);
+                    continue;
+                }
 
                 for (Map<String, Object> existingAttribute : attributes) {
                     String existingAttributeId = (String) existingAttribute.get("attributeId");
@@ -252,7 +245,6 @@ public class MyAdapter {
                         break;
                     }
                 }
-
                 if (attributeIdExists) {
                     attributeIdExists = false;
                     processApply(nestedApply.path("apply"), attributes, map);
@@ -261,7 +253,6 @@ public class MyAdapter {
 
                 Map<String, Object> attribute = new LinkedHashMap<>();
                 attribute.put("attributeId", attributeId);
-
                 Map<String, Object> attributeValue = new LinkedHashMap<>();
                 attributeValue.put("dataType", dataType);
 
@@ -284,14 +275,11 @@ public class MyAdapter {
                 }
 
                 attribute.put("attributeValue", attributeValue);
-
                 List<Map<String, Object>> attributeList = new ArrayList<>();
                 attributeList.add(attribute);
-
                 Map<String, Object> categoryMap = new LinkedHashMap<>();
                 categoryMap.put("category", category);
                 categoryMap.put("attribute", attributeList);
-
                 attributes.add(categoryMap);
 
                 processApply(nestedApply.path("apply"), attributes, map);
